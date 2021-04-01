86e764aba36b842fa99d07660d9ac100b442d3c3
Jozef Privarčák <jozef.privarcak@dataquest.sk>
2021-04-01 13:30:35 +0200

    Add some code to get error from cppcheck

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <string>
 #include <iostream>
 
+void foo(int x)
+{
+    int buf[10];
+    if (x == 1000)
+        buf[x] = 0; // <- ERROR
+}
+
 int main (){
     std::string text = "ahoj";
     std::cout << text << std::endl;
 
+
+    foo(4);
 
     return 0;
 }
